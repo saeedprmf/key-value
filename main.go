015700a85e5aa90dff48a55ee3efcc6c7e5143d7
saeedprmf <saeedpr720@gmail.com>
2026-08-25 17:55:27 -0700

    make an expection for shoting down safety

--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"key-value/configs"
 	"key-value/core"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-//TODO: make an expection for shoting down safety 
+
 //TODO: handle errors and dont use panic()
+
+
+func exit(sigchan <- chan os.Signal){
+    sig := <- sigchan
+    fmt.Println(sig)
+
+    core.ShotDown()
+
+}
+
+
 
 func main(){
     core.Log("starting app ...")
     core.Init()
     configs.Init()
 
-    core.ShotDown()
+    sigchan := make(chan os.Signal)
+    signal.Notify(sigchan , syscall.SIGINT , syscall.SIGTERM)
+
+    exit(sigchan)
 
 }
 
